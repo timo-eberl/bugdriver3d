2945a7b7f599aae02f983ea19b7cfe26461d56d6
Niklas Mäckle <niklas.maeckle@gmail.com>
2025-10-04 11:13:38 +0200

    basic vehicle setup

---
+++ Scripts/bus.gd
@@ -0,0 +1,38 @@
+extends VehicleBody3D
+
+const STEER_SPEED = 1.5
+const STEER_LIMIT = 0.4
+const BRAKE_STRENGTH = 2.0
+
+@export var engine_force_value := 40.0
+
+var previous_speed := linear_velocity.length()
+var _steer_target := 0.0
+
+
+func _physics_process(delta: float) -> void:
+	var fwd_mps := (linear_velocity * transform.basis).x
+	
+	_steer_target = Input.get_axis("turn_right", "turn_left")
+	_steer_target *= STEER_LIMIT
+
+	if Input.is_action_pressed("accelerate"):
+		var speed := linear_velocity.length()
+		if speed < 5.0 and not is_zero_approx(speed):
+			engine_force = clampf(engine_force_value * 5.0 / speed, 0.0, 100.0)
+		else:
+			engine_force = engine_force_value
+	else:
+		engine_force = 0.0
+		
+	if Input.is_action_pressed("reverse"):
+		var speed := linear_velocity.length()
+		if speed < 5.0 and not is_zero_approx(speed):
+			engine_force = -clampf(engine_force_value * BRAKE_STRENGTH * 5.0 / speed, 0.0, 100.0)
+		else:
+			engine_force = -engine_force_value * BRAKE_STRENGTH
+		
+	
+	steering = move_toward(steering, _steer_target, STEER_SPEED * delta)
+
+	previous_speed = linear_velocity.length()

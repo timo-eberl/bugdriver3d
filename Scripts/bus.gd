bea8d844c157cb0aa8a2a8161554e8d28716cc1f
Niklas Mäckle <niklas.maeckle@gmail.com>
2025-10-04 22:59:24 +0200

    vehicle adjustments

--- Scripts/bus.gd
+++ Scripts/bus.gd
@@ -8,34 +8,59 @@ signal bug_collected
 @export var STEER_LIMIT := 0.4
 @export var BRAKE_STRENGTH := 2.0
 
-var previous_speed := linear_velocity.length()
+#@export var drift_strength := 5.0
+
+@export var damping_factor := 0.7
+var slowdown_timer := 0.0
+
+@export var engine_force_curve : Curve
+
+#var previous_speed := linear_velocity.length()
 var _steer_target := 0.0
 
 
 func _physics_process(delta: float) -> void:
-	_steer_target = Input.get_axis("turn_right", "turn_left")
-	_steer_target *= STEER_LIMIT
-
 	if Input.is_action_pressed("accelerate"):
 		var speed := linear_velocity.length()
 		if speed < 5.0 and not is_zero_approx(speed):
 			engine_force = clampf(engine_force_value * 5.0 / speed, 0.0, 100.0)
 		else:
 			engine_force = engine_force_value
-	else:
+	else: 
 		engine_force = 0.0
-		
+	
 	if Input.is_action_pressed("reverse"):
 		var speed := linear_velocity.length()
 		if speed < 5.0 and not is_zero_approx(speed):
 			engine_force = -clampf(engine_force_value * BRAKE_STRENGTH * 5.0 / speed, 0.0, 100.0)
 		else:
 			engine_force = -engine_force_value * BRAKE_STRENGTH
-		
+
+	
+	_steer_target = Input.get_axis("turn_right", "turn_left")
+	_steer_target *= STEER_LIMIT
+	
+	#if Input.is_action_pressed("brake"):
+		#_steer_target *= drift_strength
 	
 	steering = move_toward(steering, _steer_target, STEER_SPEED * delta)
+	
+	if 	Input.is_action_pressed("accelerate") or \
+		Input.is_action_pressed("reverse"):
+			slowdown_timer = linear_velocity.length()
+	elif slowdown_timer > 0.0:
+		engine_force = 0.0
+		slowdown_timer -= delta * damping_factor
+	else:
+		engine_force = 0.0
+		set_linear_velocity(Vector3.ZERO)
 
-	previous_speed = linear_velocity.length()
+#func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	#if previous_speed > state.linear_velocity.length() and state.linear_velocity.length() <= 0.5: 
+		#state.set_linear_velocity(Vector3.ZERO)
+		##print("happens")
+#
+	#previous_speed = state.linear_velocity.length()
 
 
 func _on_collect_area_body_entered(body: Node3D) -> void:

f0ebe69fae3c277052dedccbe4e6271a63f91bc6
Niklas Mäckle <niklas.maeckle@gmail.com>
2025-10-06 00:49:41 +0200

    dynamic mud puddle & bouncy mushroom creation

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,9 +3,13 @@ extends Node3D
 @onready var ui : UI = $HUD/UI#TODO
 @onready var ambient_audio_player: AudioStreamPlayer3D = $AmbientAudioPlayer
 
+@onready var level: Node3D = $level
+
+@onready var mud_script : Script = preload("res://Scripts/mud.gd")
 
 func _ready() -> void:
 	ambient_audio_player.play()
+	_prepare_level()
 
 
 func _on_bus_bug_collected() -> void:
@@ -19,3 +23,20 @@ func _on_ui_round_over() -> void:
 
 #func _on_ui_round_start() -> void:
 	#ambient_audio_player.stop() 
+
+
+func _prepare_level() -> void:
+	for node in level.get_children():
+		if node.name.contains("puddle"):
+			node.set_script(mud_script)
+			var area_3d = Area3D.new()
+			node.add_child(area_3d)
+			area_3d.set_collision_mask_value(2, true)
+			var shape = node.get_node("StaticBody3D/CollisionShape3D")
+			shape.reparent(area_3d)
+			
+			node.area = area_3d
+			node.connect_signals()
+
+		if node.name.contains("mushroom"):
+			node.get_child(0).add_to_group("Mushroom")
--- Scripts/mud.gd
+++ Scripts/mud.gd
@@ -3,10 +3,9 @@ extends MeshInstance3D
 @export var area : Area3D
 
 
-func _ready() -> void:
-	area.connect("body_entered", _on_mud_entered)
-	area.connect("body_exited", _on_mud_exited)
-	
+func connect_signals() -> void:
+	area.body_entered.connect(_on_mud_entered)
+	area.body_exited.connect(_on_mud_exited)
 
 func _on_mud_entered(body : Node3D) -> void:
 	if body is Bus:

4454108d48423a9cd28861f4f30948a98e793260
Niklas Mäckle <niklas.maeckle@gmail.com>
2025-10-06 00:56:40 +0200

    weaker boost in mud

--- Scripts/bus.gd
+++ Scripts/bus.gd
@@ -128,7 +128,10 @@ func _physics_process(delta: float) -> void:
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 	if Input.is_action_pressed("boost"):
-		state.apply_central_force((self.global_basis * -Vector3.FORWARD) * 3000.0)
+		if not status_effects[StatusEffects.StatusEffect.MUD].is_empty():
+			state.apply_central_force((self.global_basis * -Vector3.FORWARD) * 1000.0)
+		else:
+			state.apply_central_force((self.global_basis * -Vector3.FORWARD) * 3000.0)
 	
 	state.linear_velocity = linear_velocity.limit_length(MAX_SPEED)
 	if slowdown_timer < 0.0 and linear_velocity.length() < 1.0:

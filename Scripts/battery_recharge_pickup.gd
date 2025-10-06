15cbc1f5ba34b9bb4a688c38ce301ca6c9c8885e
Niklas Mäckle <niklas.maeckle@gmail.com>
2025-10-06 02:08:06 +0200

    battery recharge pickup

---
+++ Scripts/battery_recharge_pickup.gd
@@ -0,0 +1,10 @@
+extends MeshInstance3D
+
+@export_range(0.0, 1.0, 0.01) var charge_amount := 0.1
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body is Bus:
+		var bus = body as Bus
+		bus.recharge_battery(charge_amount)
+		
+		queue_free()

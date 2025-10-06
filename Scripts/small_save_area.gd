a80a971e5b8415f9be3dcae48c36b2fc8dddca48
Timo Eberl <timo@timoeberl.de>
2025-10-06 02:05:32 +0200

    fix bug where you would get infinite points for a single bug

--- Scripts/small_save_area.gd
+++ Scripts/small_save_area.gd
@@ -5,5 +5,6 @@ signal bug_saved(type: Bug.BugType)
 func _on_body_entered(body: Node3D) -> void:
 	if body is Bug:
 		var bug := body as Bug
-		emit_signal("bug_saved", bug.type)
+		if !bug.saved_idle:
+			emit_signal("bug_saved", bug.type)
 		bug.save_idle()

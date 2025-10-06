92cb84fedab4e7c7b0b472bbceb0e0df0b8c5e77
Hannula <r.p.hannula@gmail.com>
2025-10-06 23:09:45 +0300

    Implement the rest of the voices

--- Scripts/audio_player.gd
+++ Scripts/audio_player.gd
@@ -1,5 +1,11 @@
 extends Node
 
+var last_rescue_timer = 0.0
+
+func _process(delta: float) -> void:
+	if last_rescue_timer > 0:
+		last_rescue_timer -= delta
+
 func play_sound(pos : Vector3, sound : AudioStreamWAV, volume_db : float = 0.0, pitch_scale : float = 1.0, unit_size: float = 70) -> void:
 	var audiostream = AudioStreamPlayer3D.new()
 	add_child(audiostream)
@@ -28,3 +34,20 @@ func play_sound_global(sound : AudioStreamWAV, volume_db : float = 0.0, pitch_sc
 	
 	await  audiostream.finished
 	audiostream.queue_free()
+
+func play_rescue_sound(sound : AudioStreamWAV) -> void:
+	if last_rescue_timer <= 0:
+		last_rescue_timer = 2.0
+		var audiostream = AudioStreamPlayer3D.new()
+		add_child(audiostream)
+		audiostream.global_position = Vector3(-40, 1, 40)
+		
+		audiostream.stream = sound
+		audiostream.volume_db = -15
+		audiostream.pitch_scale = randf_range(1.1, 1.5)
+		audiostream.unit_size = 50
+		audiostream.play()
+
+		
+		await  audiostream.finished
+		audiostream.queue_free()

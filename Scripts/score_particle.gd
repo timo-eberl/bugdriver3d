6119332cd82e64736bcb171876dffe59e30b0d87
Hannula <r.p.hannula@gmail.com>
2025-10-07 01:50:08 +0300

    Revert "Adjust music volume levels"
    This reverts commit d95e13d39707166079ed6e68425f46b8c51ab907.

--- Scripts/score_particle.gd
+++ Scripts/score_particle.gd
@@ -4,10 +4,7 @@ class_name ScoreParticle
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 func emit() -> void:
-	randomize()
-	var random = randi_range(0, 2)
-	
-	animation_player.play(str("emit_" + str(random)))
+	animation_player.play("emit")
 	
 	await animation_player.animation_finished
 	
